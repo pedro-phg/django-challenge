dd7f2e1939178e7ce83634898f4aaff3d1033493
Pedro Adalberto <pedro.h.adalberto@gmail.com>
2023-03-15 13:58:14 -0300

    Iniciando Desafio: Configurações iniciais, agora é com você!

--- challenge/views.py
+++ challenge/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'challenge/home.html', context={
+        'user': 'Julia'
+    })
+
+
+def desafio(request):
+    return HttpResponse("Estamos na página desafio.")
